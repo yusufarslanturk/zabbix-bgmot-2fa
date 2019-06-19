085115ebacfd5acba849365b04080d34ed58e1c7
Natalja Karpinska <natalja.karpinska@zabbix.com>
2019-06-19 10:12:15 +0300

    .......... [ZBXNEXT-826] updated release version [4.2.4rc2]

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,7 +19,7 @@
 **/
 
 
-define('ZABBIX_VERSION',		'4.2.4rc1');
+define('ZABBIX_VERSION',		'4.2.4rc2');
 define('ZABBIX_API_VERSION',	'4.2.4');
 define('ZABBIX_EXPORT_VERSION',	'4.2');
 define('ZABBIX_DB_VERSION',	4020000);
