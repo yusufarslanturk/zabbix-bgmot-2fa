626e4cf5989b7063ad24774ca62dca5cf593bfc6
Andrejs Kozlovs <andrejs.kozlovs@zabbix.com>
2019-06-20 09:09:09 +0300

    .......PS. [ZBX-16263] fixed preprocessing step history is not resetted when 'on fail' 'set value' action triggers

--- src/zabbix_server/preprocessor/preproc_worker.c
+++ src/zabbix_server/preprocessor/preproc_worker.c
@@ -229,6 +229,8 @@ static int	worker_item_preproc_execute(unsigned char value_type, zbx_variant_t *
 		{
 			results[i].action = op->error_handler;
 			ret = zbx_item_preproc_handle_error(value, op, error);
+			zbx_variant_clear(&history_value);
+			break;
 		}
 		else
 			results[i].action = ZBX_PREPROC_FAIL_DEFAULT;
@@ -254,7 +256,6 @@ static int	worker_item_preproc_execute(unsigned char value_type, zbx_variant_t *
 
 		if (SUCCEED != ret)
 		{
-			zbx_variant_clear(&history_value);
 			break;
 		}
 
