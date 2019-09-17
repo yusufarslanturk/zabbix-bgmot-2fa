69380cbb65bd9bdedc127c2056c518a483a26c92
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2019-09-17 15:10:08 +0300

    A......... [ZBX-16349] fixed XML import error when updating a discovery rule with an item prototype having a new key
    * commit '579dc098e2e8c50999dafcff1879f0971d6dd60c':
      A......... [ZBX-16349] fixed XML import error when updating a discovery rule with an item prototype having a new key
    
    (cherry picked from commit ec2791294eeaca05a9a77725bd723ee22f8d108e)

--- frontends/php/include/classes/import/CConfigurationImport.php
+++ frontends/php/include/classes/import/CConfigurationImport.php
@@ -2198,6 +2198,8 @@ class CConfigurationImport {
 
 		if ($del_item_prototypes) {
 			API::ItemPrototype()->delete(array_keys($del_item_prototypes));
+
+			$this->referencer->refreshGraphs();
 		}
 	}
 
