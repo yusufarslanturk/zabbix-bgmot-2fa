1410c54963bfb448a074af810a8effbba19ba5f8
Roberts Lataria <roberts.lataria@zabbix.com>
2019-12-06 09:42:27 +0200

    ..F....... [ZBX-16918] fixed typo

--- frontends/php/js/dashboard.grid.js
+++ frontends/php/js/dashboard.grid.js
@@ -2370,7 +2370,7 @@
 					widget['fields'] = fields;
 					widget['preloader_timeout'] = 100;
 
-					// Set preloader to widget contant after overlayDialogueDestroy as fast as we can.
+					// Set preloader to widget content after overlayDialogueDestroy as fast as we can.
 					startPreloader(widget);
 
 					// View mode was just set after the overlayDialogueDestroy was called in first 'then' section.
