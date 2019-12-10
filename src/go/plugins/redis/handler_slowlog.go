2327830e3d9fbcd999de5f839c5752671d159ec4
Vadim Ipatov <vadim.ipatov@zabbix.com>
2019-12-10 10:56:02 +0300

    .......... [ZBXNEXT-5428] Simplified getLastSlowlogId

--- src/go/plugins/redis/handler_slowlog.go
+++ src/go/plugins/redis/handler_slowlog.go
@@ -21,7 +21,6 @@ package redis
 
 import (
 	"github.com/mediocregopher/radix/v3"
-	"reflect"
 )
 
 type slowlog []interface{}
@@ -33,21 +32,21 @@ func getLastSlowlogId(sl slowlog) (int64, error) {
 		return 0, nil
 	}
 
-	if reflect.TypeOf(sl[0]).Kind().String() != "slice" {
+	item, ok := sl[0].(logItem)
+	if !ok {
 		return 0, errorCannotParseData
 	}
-
-	item := sl[0].(logItem)
 
 	if len(item) == 0 {
 		return 0, errorCannotParseData
 	}
 
-	if reflect.TypeOf(item[0]).Name() != "int64" {
+	id, ok := item[0].(int64)
+	if !ok {
 		return 0, errorCannotParseData
 	}
 
-	return item[0].(int64) + 1, nil
+	return id + 1, nil
 }
 
 // slowlogHandler gets an output of 'SLOWLOG GET 1' command and returns the last slowlog Id.
