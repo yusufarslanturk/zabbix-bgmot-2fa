2a4b0743f017838301f205885e750ea85b834def
Ivo Kurzemnieks <ivo.kurzemnieks@zabbix.com>
2020-02-21 11:20:58 +0200

    ..F....... [ZBX-16112] minor code refactoring

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -67,8 +67,6 @@ define('ZBX_FONTPATH',				realpath('assets/fonts')); // where to search for font
 define('ZBX_GRAPH_FONT_NAME',		'DejaVuSans'); // font file name
 define('ZBX_GRAPH_LEGEND_HEIGHT',	120); // when graph height is less then this value, some legend will not show up
 
-define('ZBX_SCRIPT_TIMEOUT',		60); // in seconds
-
 define('GRAPH_YAXIS_SIDE_DEFAULT', 0); // 0 - LEFT SIDE, 1 - RIGHT SIDE
 
 define('ZBX_MAX_IMAGE_SIZE', ZBX_MEBIBYTE);
@@ -1211,6 +1209,7 @@ define('ZBX_DISCOVERER_IPRANGE_LIMIT', 65536);
 define('ZBX_SOCKET_TIMEOUT',			3);		// Socket timeout limit.
 define('ZBX_CONNECT_TIMEOUT',			3);		// Zabbix server connect timeout limit.
 define('ZBX_MEDIA_TYPE_TEST_TIMEOUT',	65);	// Timeout limit set for media type test.
+define('ZBX_SCRIPT_TIMEOUT',			60);	// Timeout limit set for scripts.
 define('ZBX_SOCKET_BYTES_LIMIT',    ZBX_MEBIBYTE * 16); // socket response size limit
 
 // value is also used in servercheck.js file
