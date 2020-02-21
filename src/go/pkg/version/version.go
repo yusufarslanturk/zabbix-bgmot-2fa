40e3f88057d47ca471a0f8645f859e3dea8ee7ee
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2020-02-21 10:56:56 +0200

    .......... [ZBXNEXT-826] updated release date and version [4.4.6]

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -33,7 +33,7 @@ const (
 	ZABBIX_VERSION_MAJOR    = 4
 	ZABBIX_VERSION_MINOR    = 4
 	ZABBIX_VERSION_PATCH    = 6
-	ZABBIX_VERSION_RC       = "rc1"
+	ZABBIX_VERSION_RC       = ""
 	ZABBIX_VERSION_RC_NUM   = "{ZABBIX_RC_NUM}"
 	ZABBIX_VERSION_REVISION = "{ZABBIX_REVISION}"
 	copyrightMessage        = "Copyright (C) 2020 Zabbix SIA\n" +
