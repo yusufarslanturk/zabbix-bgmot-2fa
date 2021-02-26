e519943383414bf53de952fa951a02150a770e89
Evgeny Yurchenko <ey@bgmot.com>
2021-02-25 19:35:49 -0500

    Bring ui/conf/maintenance.inc.php to upstream version.

--- ui/conf/maintenance.inc.php
+++ ui/conf/maintenance.inc.php
@@ -20,13 +20,10 @@
 
 
 // Maintenance mode
-if (getenv('ZBX_DENY_GUI_ACCESS') == 'true') {
-    define('ZBX_DENY_GUI_ACCESS', 1);
+//define('ZBX_DENY_GUI_ACCESS', 1);
 
-    // IP range, who are allowed to connect to FrontEnd
-    $ip_range = str_replace("'","\"",getenv('ZBX_GUI_ACCESS_IP_RANGE'));
-    $ZBX_GUI_ACCESS_IP_RANGE = (json_decode($ip_range)) ? json_decode($ip_range, true) : array();
+// IP range, who are allowed to connect to FrontEnd
+//$ZBX_GUI_ACCESS_IP_RANGE = array('127.0.0.1');
 
-    // MSG shown on Warning screen!
-    $_REQUEST['warning_msg'] = getenv('ZBX_GUI_WARNING_MSG');
-}
+// MSG shown on Warning screen!
+//$_REQUEST['warning_msg'] = 'Zabbix is under maintenance.';
