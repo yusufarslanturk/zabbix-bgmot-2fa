b4664abd7cdc38ff33615936eba58877320d210b
Evgeny Yurchenko <ey@bgmot.com>
2021-02-25 19:55:53 -0500

    bg-scripts/bg-features-install.sh updated with 5.0.9.

--- bg-scripts/bg-features-install.sh
+++ bg-scripts/bg-features-install.sh
@@ -11,8 +11,8 @@ DB_PASSWORD=zabbix
 ZABBIX_INSTALL_PATH=/usr/share/zabbix
 
 TMPDIR=/tmp/`date +%s`
-VERSION=5.0.8
-BGVERSION=5.0.8-bg
+VERSION=5.0.9
+BGVERSION=5.0.9-bg
 ZABBIX_INSTALLED_VERSION=`grep ZABBIX_VERSION ${ZABBIX_INSTALL_PATH}/include/defines.inc.php | cut -d\' -f 4`
 
 if [ "$VERSION" != "$ZABBIX_INSTALLED_VERSION" ]
