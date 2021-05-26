531377c23ce8b110a6904863983fc2022d1cd20a
Sergejs Olonkins <sergejs.olonkins@zabbix.com>
2021-05-26 13:46:25 +0300

    .......... [ZBXNEXT-826] updated release version [4.0.32rc1]; updated API version [4.0.32]

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,8 +19,8 @@
 **/
 
 
-define('ZABBIX_VERSION',		'4.0.31');
-define('ZABBIX_API_VERSION',	'4.0.31');
+define('ZABBIX_VERSION',		'4.0.32rc1');
+define('ZABBIX_API_VERSION',	'4.0.32');
 define('ZABBIX_EXPORT_VERSION',	'4.0');
 define('ZABBIX_DB_VERSION',		4000000);
 
