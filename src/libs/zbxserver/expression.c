5da2f007e13cc4e294b7dbb97ec5d4fa14bfe375
Aleksejs Sestakovs <aleksejs.sestakovs@zabbix.com>
2021-10-05 22:20:36 +0300

    ........S. [ZBX-19872] removed redundant escaping in XML data of HTTP agent POST request

--- src/libs/zbxserver/expression.c
+++ src/libs/zbxserver/expression.c
@@ -4215,15 +4215,6 @@ int	substitute_simple_macros(zbx_uint64_t *actionid, const DB_EVENT *event, cons
 		if (0 != (macro_type & MACRO_TYPE_HTTP_JSON) && NULL != replace_to)
 			zbx_json_escape(&replace_to);
 
-		if (0 != (macro_type & MACRO_TYPE_HTTP_XML) && NULL != replace_to)
-		{
-			char	*replace_to_esc;
-
-			replace_to_esc = xml_escape_dyn(replace_to);
-			zbx_free(replace_to);
-			replace_to = replace_to_esc;
-		}
-
 		if (ZBX_TOKEN_FUNC_MACRO == token.type && NULL != replace_to)
 		{
 			if (SUCCEED != (ret = zbx_calculate_macro_function(*data, &token.data.func_macro, &replace_to)))
@@ -5250,14 +5241,6 @@ static int	process_lld_macro_token(char **data, zbx_token_t *token, int flags, c
 	{
 		zbx_json_escape(&replace_to);
 	}
-	else if (0 != (flags & ZBX_TOKEN_XML))
-	{
-		char	*replace_to_esc;
-
-		replace_to_esc = xml_escape_dyn(replace_to);
-		zbx_free(replace_to);
-		replace_to = replace_to_esc;
-	}
 	else if (0 != (flags & ZBX_TOKEN_REGEXP))
 	{
 		zbx_regexp_escape(&replace_to);
@@ -5792,9 +5775,10 @@ static void	substitute_macros_in_xml_elements(const DC_ITEM *item, const struct 
 							NULL, &value_tmp, MACRO_TYPE_HTTP_XML, NULL, 0);
 				}
 				else
-					substitute_lld_macros(&value_tmp, jp_row, ZBX_MACRO_XML, NULL, 0);
+					substitute_lld_macros(&value_tmp, jp_row, ZBX_MACRO_ANY, NULL, 0);
 
-				xmlNodeSetContent(node, (xmlChar *)value_tmp);
+				xmlNodeSetContent(node, (xmlChar *)"");
+				xmlNodeAddContent(node, (xmlChar *)value_tmp);
 
 				zbx_free(value_tmp);
 				xmlFree(value);
@@ -5813,7 +5797,8 @@ static void	substitute_macros_in_xml_elements(const DC_ITEM *item, const struct 
 				else
 					substitute_lld_macros(&value_tmp, jp_row, ZBX_MACRO_ANY, NULL, 0);
 
-				xmlNodeSetContent(node, (xmlChar *)value_tmp);
+				xmlNodeSetContent(node, (xmlChar *)"");
+				xmlNodeAddContent(node, (xmlChar *)value_tmp);
 
 				zbx_free(value_tmp);
 				xmlFree(value);
@@ -5833,7 +5818,7 @@ static void	substitute_macros_in_xml_elements(const DC_ITEM *item, const struct 
 								NULL, 0);
 					}
 					else
-						substitute_lld_macros(&value_tmp, jp_row, ZBX_MACRO_XML, NULL, 0);
+						substitute_lld_macros(&value_tmp, jp_row, ZBX_MACRO_ANY, NULL, 0);
 
 					xmlSetProp(node, attr->name, (xmlChar *)value_tmp);
 
