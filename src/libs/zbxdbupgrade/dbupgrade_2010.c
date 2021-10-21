e205f357b43c4d86a0b9cadc0e423a539849a0d6
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2021-10-21 16:26:04 +0300

    .......... [DEV-1977] added check for quote_key_param

--- src/libs/zbxdbupgrade/dbupgrade_2010.c
+++ src/libs/zbxdbupgrade/dbupgrade_2010.c
@@ -946,7 +946,7 @@ static int	DBpatch_2010101(void)
 
 			if (1 != num_param(param))
 			{
-				if (FAIL == quote_key_param(&param, 0))
+				if (FAIL == (ret = quote_key_param(&param, 0)))
 					error_message = zbx_dsprintf(error_message, "unique description"
 							" \"%s\" contains invalid symbols and cannot be quoted", param);
 			}
