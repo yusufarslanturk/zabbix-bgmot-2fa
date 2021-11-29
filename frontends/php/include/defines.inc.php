cee3fa1dc3e5095a7c50d8092bd6cfd6034f8558
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2021-11-29 10:23:12 +0200

    .......... [ZBXNEXT-826] updated release date and version [4.0.36]

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,7 +19,7 @@
 **/
 
 
-define('ZABBIX_VERSION',		'4.0.36rc1');
+define('ZABBIX_VERSION',		'4.0.36');
 define('ZABBIX_API_VERSION',	'4.0.36');
 define('ZABBIX_EXPORT_VERSION',	'4.0');
 define('ZABBIX_DB_VERSION',		4000000);
