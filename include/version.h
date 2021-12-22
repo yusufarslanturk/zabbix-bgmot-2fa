d4918595436cdd1d4a1218d3feb8cc9a8d3a7c6e
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2021-12-22 16:44:14 +0200

    .......... [ZBXNEXT-826] updated release date [4.0.37rc1]

--- include/version.h
+++ include/version.h
@@ -24,7 +24,7 @@
 #define ZBX_STR(str)	ZBX_STR2(str)
 
 #define APPLICATION_NAME	"Zabbix Agent"
-#define ZABBIX_REVDATE		"29 November 2021"
+#define ZABBIX_REVDATE		"22 December 2021"
 #define ZABBIX_VERSION_MAJOR	4
 #define ZABBIX_VERSION_MINOR	0
 #define ZABBIX_VERSION_PATCH	37
