a76f1d7b0787c28add1ace936e476ea1f42e8240
Andrejs Verza <andrejs.verza@zabbix.com>
2021-12-23 10:29:11 +0200

    ..F.I..... [ZBX-20387] fixed default language of the setup routine for logged in superadmin users

--- frontends/php/setup.php
+++ frontends/php/setup.php
@@ -58,19 +58,30 @@ $fields = [
 ];
 
 CSession::start();
+CSession::setValue('check_fields_result', check_fields($fields, false));
+if (!CSession::keyExists('step')) {
+	CSession::setValue('step', 0);
+}
 
-$check_fields_result = check_fields($fields, false);
-
-if (hasRequest('cancel') || hasRequest('finish')) {
+// if a guest or a non-super admin user is logged in
+if (CWebUser::$data && CWebUser::getType() < USER_TYPE_SUPER_ADMIN) {
+	// on the last step of the setup we always have a guest user logged in;
+	// when he presses the "Finish" button he must be redirected to the login screen
+	if (CWebUser::isGuest() && CSession::getValue('step') == 5 && hasRequest('finish')) {
+		CSession::clear();
+		redirect('index.php');
+	}
+	// the guest user can also view the last step of the setup
+	// all other user types must not have access to the setup
+	elseif (!(CWebUser::isGuest() && CSession::getValue('step') == 5)) {
+		access_deny(ACCESS_DENY_PAGE);
+	}
+}
+// if a super admin or a non-logged in user presses the "Finish" or "Login" button - redirect him to the login screen
+elseif (hasRequest('cancel') || hasRequest('finish')) {
 	CSession::clear();
 	redirect('index.php');
 }
-
-if (CWebUser::$data && CWebUser::getType() < USER_TYPE_SUPER_ADMIN && CSession::getValue('step') != 5) {
-	access_deny(ACCESS_DENY_PAGE);
-}
-
-CSession::setValue('check_fields_result', $check_fields_result);
 
 $theme = CWebUser::$data ? getUserTheme(CWebUser::$data) : ZBX_DEFAULT_THEME;
 
@@ -81,13 +92,18 @@ DBclose();
  */
 $ZBX_SETUP_WIZARD = new CSetupWizard();
 
+// if init fails due to missing configuration, set user as guest with default en_GB language
+if (!CWebUser::$data) {
+	CWebUser::setDefault();
+}
+
 // page title
 (new CPageHeader(_('Installation')))
 	->addCssFile('assets/styles/'.CHtml::encode($theme).'.css')
 	->addJsFile((new CUrl('js/browsers.js'))->getUrl())
 	->addJsFile((new CUrl('jsLoader.php'))
 		->setArgument('ver', ZABBIX_VERSION)
-		->setArgument('lang', 'en_gb')
+		->setArgument('lang', CWebUser::$data['lang'])
 		->getUrl()
 	)
 	->display();
