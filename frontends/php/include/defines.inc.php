5c6d4bf1886a6adb0fd3dde2e404400c1ec3cdfa
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2021-12-23 19:14:33 +0200

    .......... [ZBXNEXT-826] updated release version [4.0.38rc1]; updated API version [4.0.38]

--- frontends/php/include/defines.inc.php
+++ frontends/php/include/defines.inc.php
@@ -19,8 +19,8 @@
 **/
 
 
-define('ZABBIX_VERSION',		'4.0.37');
-define('ZABBIX_API_VERSION',	'4.0.37');
+define('ZABBIX_VERSION',		'4.0.38rc1');
+define('ZABBIX_API_VERSION',	'4.0.38');
 define('ZABBIX_EXPORT_VERSION',	'4.0');
 define('ZABBIX_DB_VERSION',		4000000);
 
