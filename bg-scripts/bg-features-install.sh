581c73b468d0abbcc9e87540fed20f60b1f73e9a
Evgeny Yurchenko <ey@bgmot.com>
2022-02-24 19:24:49 -0500

    bg-scripts/bg-features-install.sh updated to use archive of patched files.

--- bg-scripts/bg-features-install.sh
+++ bg-scripts/bg-features-install.sh
@@ -7,11 +7,11 @@ set -o
 DB_HOST=localhost
 DB_USERNAME=zabbix
 DB_PASSWORD=zabbix
+DB_NAME=zabbix
 ZABBIX_INSTALL_PATH=/usr/share/zabbix
 
 TMPDIR=/tmp/`date +%s`
 VERSION=5.4.10
-BGVERSION=5.4.10-bg
 ZABBIX_INSTALLED_VERSION=`grep ZABBIX_VERSION ${ZABBIX_INSTALL_PATH}/include/defines.inc.php | cut -d\' -f 4`
 
 if [ "$VERSION" != "$ZABBIX_INSTALLED_VERSION" ]
@@ -21,18 +21,18 @@ then
 fi
 
 mkdir $TMPDIR && cd $TMPDIR
-curl -L -o zabbix-${BGVERSION}.zip https://github.com/BGmot/zabbix/archive/${BGVERSION}.zip
-unzip zabbix-${BGVERSION}.zip
-cd zabbix-${BGVERSION}
+curl -L -o bg-patch-${VERSION}.zip https://github.com/BGmot/zabbix/raw/release/5.4-bg/bg-patch-${VERSION}.zip
+unzip bg-patch-${VERSION}.zip
+cd bg-patch-${VERSION}
 
 # Check if DB is already patched
-echo 'show tables' | mysql -u $DB_USERNAME -p${DB_PASSWORD} -h ${DB_HOST} zabbix | grep adusrgrp
+echo 'show tables' | mysql -u $DB_USERNAME -p${DB_PASSWORD} -h ${DB_HOST} ${DB_NAME} | grep adusrgrp
 if [ "$?" -eq "0" ]
 then
   echo 'Database already patched. No changes to DB will be made'
 else
   echo 'Patching DB...'
-  mysql -u $DB_USERNAME -p${DB_PASSWORD} -h ${DB_HOST} zabbix < bg-scripts/db-update.sql
+  mysql -u $DB_USERNAME -p${DB_PASSWORD} -h ${DB_HOST} ${DB_NAME} < bg-scripts/db-update.sql
   if [ "$?" -eq "0" ]
   then
     echo 'Database was successfully patched.'
@@ -54,7 +54,10 @@ cp assets/styles/Duo-Frame.css ${ZABBIX_INSTALL_PATH}/assets/styles/
 cp js/Duo-Web-v2.js ${ZABBIX_INSTALL_PATH}/js
 cd include
 cp ${ZABBIX_INSTALL_PATH}/include/defines.inc.php ${ZABBIX_INSTALL_PATH}/include/defines.inc.php-`date +%s`.bak
-cp defines.inc.php ${ZABBIX_INSTALL_PATH}/include/
+if [ -z "$(grep ZBX_AUTH_2FA_NONE ${ZABBIX_INSTALL_PATH}/include/defines.inc.php)" ]; then echo "define('ZBX_AUTH_2FA_NONE', 0);" >> ${ZABBIX_INSTALL_PATH}/include/defines.inc.php; fi
+if [ -z "$(grep ZBX_AUTH_2FA_DUO ${ZABBIX_INSTALL_PATH}/include/defines.inc.php)" ]; then echo "define('ZBX_AUTH_2FA_DUO',    1);" >> ${ZABBIX_INSTALL_PATH}/include/defines.inc.php; fi
+if [ -z "$(grep ZBX_AUTH_2FA_GGL ${ZABBIX_INSTALL_PATH}/include/defines.inc.php)" ]; then echo "define('ZBX_AUTH_2FA_GGL',    2);" >> ${ZABBIX_INSTALL_PATH}/include/defines.inc.php; fi
+if [ -z "$(grep AUDIT_RESOURCE_AD_GROUP ${ZABBIX_INSTALL_PATH}/include/defines.inc.php)" ]; then echo "define('AUDIT_RESOURCE_AD_GROUP',    100);" >> ${ZABBIX_INSTALL_PATH}/include/defines.inc.php; fi
 cp ${ZABBIX_INSTALL_PATH}/include/perm.inc.php ${ZABBIX_INSTALL_PATH}/include/perm.inc.php-`date +%s`.bak
 cp perm.inc.php ${ZABBIX_INSTALL_PATH}/include
 cp ${ZABBIX_INSTALL_PATH}/include/schema.inc.php ${ZABBIX_INSTALL_PATH}/include/schema.inc.php-`date +%s`.bak
