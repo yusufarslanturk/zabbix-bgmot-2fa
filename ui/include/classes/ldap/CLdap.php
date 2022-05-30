e95cd6e6adba6ac6de145c36caf65be7d7387451
sergii.molchanov <sergii.molchanov@adstream.com>
2022-05-30 14:22:47 +0100

    LDAP auth fix to work with search_attribute

--- ui/include/classes/ldap/CLdap.php
+++ ui/include/classes/ldap/CLdap.php
@@ -228,21 +228,14 @@ class CLdap {
 		else {
 			$filter = '(ObjectClass=*)';
 		}
-		$sr = @ldap_search($this->ds, $base, $filter, ['cn','memberof', 'mail']);
+		$attr = strtolower($this->cnf['search_attribute']);
+		$sr = @ldap_search($this->ds, $base, $filter, ['cn','memberof', 'mail', $attr]);
 		$result = is_resource($sr) ? @ldap_get_entries($this->ds, $sr) : [];
 
 		// don't accept more or less than one response
-		if ($this->cnf['search_attribute'] == 'uid'){
-			# OpenLDAP
-			$attr = 'cn';
-		} else {
-			# Active Directory
-			$attr = strtolower($this->cnf['search_attribute']);
-		}
 		if (!$result || $result['count'] != 1 ||
-		   ($this->cnf['case_sensitive'] == '1' && $result[0][$attr][0] != $user)) {
+		($this->cnf['case_sensitive'] == '1' && $result[0][$attr][0] != $user)) {
 			$this->error = $result ? static::ERR_USER_NOT_FOUND : static::ERR_BIND_FAILED;
-
 			return false;
 		}
 
