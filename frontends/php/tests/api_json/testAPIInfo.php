8b4a1fe8c025afa6c8251e8dba0f84498915b7b9
Sergejs Maklakovs <sergejs.maklakovs@zabbix.com>
2022-06-01 10:24:12 +0300

    .......... [ZBXNEXT-826] updated release version [4.0.42rc1]; updated API version [4.0.42]

--- frontends/php/tests/api_json/testAPIInfo.php
+++ frontends/php/tests/api_json/testAPIInfo.php
@@ -36,6 +36,6 @@ class testAPIInfo extends CAPITest {
 		$this->disableAuthorization();
 		$result = $this->call('apiinfo.version', []);
 
-		$this->assertSame('4.0.41', $result['result']);
+		$this->assertSame('4.0.42', $result['result']);
 	}
 }
