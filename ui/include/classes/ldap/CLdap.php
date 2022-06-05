c4c09ebb693b7f5fd7b893f87da1ee1a2944a196
Evgeny Yurchenko <ey@bgmot.com>
2022-06-05 12:57:35 -0400

    Fix LDAP authentication with OpenLDAP server. include/classes/ldap/CLdap.php: fix for PHP 8.

--- ui/include/classes/ldap/CLdap.php
+++ ui/include/classes/ldap/CLdap.php
@@ -228,12 +228,19 @@ class CLdap {
 		else {
 			$filter = '(ObjectClass=*)';
 		}
-		$sr = @ldap_search($this->ds, $base, $filter);
-		$result = is_resource($sr) ? @ldap_get_entries($this->ds, $sr) : [];
+		if ($this->cnf['search_attribute'] == 'uid'){
+			# OpenLDAP
+			$attr = 'cn';
+		} else {
+			# Active Directory
+			$attr = strtolower($this->cnf['search_attribute']);
+		}
+		$sr = @ldap_search($this->ds, $base, $filter, [$attr, 'memberof', 'mail']);
+		$result = $sr ? @ldap_get_entries($this->ds, $sr) : [];
 
 		// don't accept more or less than one response
 		if (!$result || $result['count'] != 1 ||
-		   ($this->cnf['case_sensitive'] == '1' && $result[0]['cn'][0] != $user)) {
+		   ($this->cnf['case_sensitive'] == '1' && $result[0][$attr][0] != $user)) {
 			$this->error = $result ? static::ERR_USER_NOT_FOUND : static::ERR_BIND_FAILED;
 
 			return false;
@@ -247,7 +254,7 @@ class CLdap {
 		$info['name'] = $user_result['cn'][0];
 		$info['grps'] = [];
 
-		// overwrite if other attribs are specified.
+		// overwrite if other attributes are specified.
 		if (is_array($this->cnf['mapping'])) {
 			foreach ($this->cnf['mapping'] as $localkey => $key) {
 				$info[$localkey] = isset($user_result[$key])?$user_result[$key][0]:null;
