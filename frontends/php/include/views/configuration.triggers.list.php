zabbix/zabbix

..F....... [ZBX-22386] removed double-encoding by using CHtml::encode and CHtml::serialize functions

ui/include/views/configuration.triggers.list.php
@@ -236,7 +236,7 @@ foreach ($data['triggers'] as $tnum => $trigger) {
 
 	if ($trigger['discoveryRule']) {
 		$description[] = (new CLink(
-			CHtml::encode($trigger['discoveryRule']['name']),
+			$trigger['discoveryRule']['name'],
 			(new CUrl('trigger_prototypes.php'))
 				->setArgument('parent_discoveryid', $trigger['discoveryRule']['itemid'])
 				->setArgument('context', $data['context'])
@@ -247,7 +247,7 @@ foreach ($data['triggers'] as $tnum => $trigger) {
 	}
 
 	$description[] = (new CLink(
-		CHtml::encode($trigger['description']),
+		$trigger['description'],
 		(new CUrl('triggers.php'))
 			->setArgument('form', 'update')
 			->setArgument('triggerid', $triggerid)
@@ -261,9 +261,8 @@ foreach ($data['triggers'] as $tnum => $trigger) {
 		foreach ($trigger['dependencies'] as $dependency) {
 			$dep_trigger = $data['dep_triggers'][$dependency['triggerid']];
 
-			$dep_trigger_desc = CHtml::encode(
-				implode(', ', array_column($dep_trigger['hosts'], 'name')).NAME_DELIMITER.$dep_trigger['description']
-			);
+			$dep_trigger_desc =
+				implode(', ', array_column($dep_trigger['hosts'], 'name')).NAME_DELIMITER.$dep_trigger['description'];
 
 			$trigger_deps[] = (new CLink($dep_trigger_desc,
 				(new CUrl('triggers.php'))

yusufarslanturk/zabbix-bgmot-2fa

(cherry picked from commit bbd8420476b28c1a94940ca2be6ca2dba365fe48)

frontends/php/include/views/configuration.triggers.list.php
@@ -191,7 +191,7 @@ foreach ($data['triggers'] as $tnum => $trigger) {
 
 	if ($trigger['discoveryRule']) {
 		$description[] = (new CLink(
-			CHtml::encode($trigger['discoveryRule']['name']),
+			$trigger['discoveryRule']['name'],
 			'trigger_prototypes.php?parent_discoveryid='.$trigger['discoveryRule']['itemid']))
 			->addClass(ZBX_STYLE_LINK_ALT)
 			->addClass(ZBX_STYLE_ORANGE);
@@ -199,7 +199,7 @@ foreach ($data['triggers'] as $tnum => $trigger) {
 	}
 
 	$description[] = (new CLink(
-		CHtml::encode($trigger['description']),
+		$trigger['description'],
 		'triggers.php?form=update&hostid='.$data['hostid'].'&triggerid='.$triggerid
 	))->addClass(ZBX_STYLE_WORDWRAP);
 
@@ -210,9 +210,8 @@ foreach ($data['triggers'] as $tnum => $trigger) {
 		foreach ($trigger['dependencies'] as $dependency) {
 			$depTrigger = $data['dependencyTriggers'][$dependency['triggerid']];
 
-			$depTriggerDescription = CHtml::encode(
-				implode(', ', zbx_objectValues($depTrigger['hosts'], 'name')).NAME_DELIMITER.$depTrigger['description']
-			);
+			$depTriggerDescription =
+				implode(', ', zbx_objectValues($depTrigger['hosts'], 'name')).NAME_DELIMITER.$depTrigger['description'];
 
 			$triggerDependencies[] = (new CLink($depTriggerDescription,
 				'triggers.php?form=update&triggerid='.$depTrigger['triggerid']