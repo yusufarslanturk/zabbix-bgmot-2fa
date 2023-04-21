zabbix/zabbix

..F....... [ZBX-22386] replaced &hellip; entities with HELLIP() function call

ui/include/views/configuration.template.list.php
@@ -155,7 +155,7 @@ foreach ($data['templates'] as $template) {
 		$i++;
 
 		if ($i > $data['config']['max_in_table']) {
-			$linked_templates_output[] = ' &hellip;';
+			$linked_templates_output[] = [' ', HELLIP()];
 
 			break;
 		}
@@ -184,7 +184,7 @@ foreach ($data['templates'] as $template) {
 		$i++;
 
 		if ($i > $data['config']['max_in_table']) {
-			$linked_to_output[] = ' &hellip;';
+			$linked_to_output[] = [' ', HELLIP()];
 
 			break;
 		}

yusufarslanturk/zabbix-bgmot-2fa

(cherry picked from commit f7b21fd9915f5c06e28d2e2f2295f61a871ca8d9)

frontends/php/include/views/configuration.template.list.php
@@ -110,7 +110,7 @@ foreach ($data['templates'] as $template) {
 		$i++;
 
 		if ($i > $data['config']['max_in_table']) {
-			$linkedTemplatesOutput[] = ' &hellip;';
+			$linkedTemplatesOutput[] = [' ', HELLIP()];
 
 			break;
 		}
@@ -141,7 +141,7 @@ foreach ($data['templates'] as $template) {
 		$i++;
 
 		if ($i > $data['config']['max_in_table']) {
-			$linkedToOutput[] = ' &hellip;';
+			$linkedToOutput[] = [' ', HELLIP()];
 
 			break;
 		}