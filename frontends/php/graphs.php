5ca635ee68b23c80d794e88f5f1933a0618407bb
Miks Kronkalns <miks.kronkalns@zabbix.com>
2023-05-15 09:00:18 +0300

    ..F....... [DEV-2472] fixed error reporting in graph edit form

--- frontends/php/graphs.php
+++ frontends/php/graphs.php
@@ -88,9 +88,8 @@ if (isset($_REQUEST['yaxismin']) && zbx_empty($_REQUEST['yaxismin'])) {
 if (isset($_REQUEST['yaxismax']) && zbx_empty($_REQUEST['yaxismax'])) {
 	unset($_REQUEST['yaxismax']);
 }
-check_fields($fields, false);
+check_fields($fields);
 
-$color_validator = new CColorValidator();
 $gitems = [];
 
 foreach (getRequest('items', []) as $item) {
@@ -102,9 +101,8 @@ foreach (getRequest('items', []) as $item) {
 			&& array_key_exists('drawtype', $gitem) && ctype_digit($gitem['drawtype'])
 			&& array_key_exists('calc_fnc', $gitem) && ctype_digit($gitem['calc_fnc'])
 			&& array_key_exists('yaxisside', $gitem) && ctype_digit($gitem['yaxisside'])) {
-		if (array_key_exists('color', $gitem) && !$color_validator->validate($gitem['color'])) {
+		if (array_key_exists('color', $gitem)) {
 			$gitem['color'] = CHtml::encode($gitem['color']);
-			error($color_validator->getError());
 		}
 
 		$gitems[] = $gitem + ['color' => ''];
