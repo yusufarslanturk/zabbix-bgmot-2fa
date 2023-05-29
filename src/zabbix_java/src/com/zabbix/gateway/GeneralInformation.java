9046258fdbb8545d2dffab9f8143556d8188b539
Natalja Romancaka <natalja.romancaka@zabbix.com>
2023-05-29 10:16:49 +0300

    .......... [ZBXNEXT-826] updated release date and version for 4.0.46

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,9 +22,9 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	static final String REVISION_DATE = "22 May 2023";
+	static final String REVISION_DATE = "29 May 2023";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "4.0.46rc1";
+	static final String VERSION = "4.0.46";
 
 	static void printVersion()
 	{
