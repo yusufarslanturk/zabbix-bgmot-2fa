97b744aadf59842c65dc2ec17162ea4400623684
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2023-08-01 15:03:15 +0300

    ..F....... [ZBX-22027] reverted changes in fc0a9fb961b

--- ui/widgets/trigover/includes/ViewHelper.php
+++ ui/widgets/trigover/includes/ViewHelper.php
@@ -50,6 +50,7 @@ class ViewHelper {
 			->addClass(CSeverityHelper::getStyle((int) $trigger['priority'], $trigger['value'] == TRIGGER_VALUE_TRUE))
 			->addClass(ZBX_STYLE_CURSOR_POINTER);
 
+		$eventid = 0;
 		$blink_period = timeUnitToSeconds(CSettingsHelper::get(CSettingsHelper::BLINK_PERIOD));
 		$duration = time() - $trigger['lastchange'];
 
@@ -59,13 +60,21 @@ class ViewHelper {
 			$column->setAttribute('data-toggle-class', ZBX_STYLE_BLINK_HIDDEN);
 		}
 
+		if ($trigger['value'] == TRIGGER_VALUE_TRUE) {
+			$eventid = $trigger['problem']['eventid'];
+			$show_update_problem = true;
+		}
+		else {
+			$show_update_problem = false;
+		}
+
 		$column->setMenuPopup(CMenuPopupHelper::getTrigger([
 			'triggerid' => $trigger['triggerid'],
 			'backurl' => (new CUrl('zabbix.php'))
 				->setArgument('action', 'dashboard.view')
 				->getUrl(),
-			'eventid' => $trigger['problem']['eventid'],
-			'show_update_problem' => true
+			'eventid' => $eventid,
+			'show_update_problem' => $show_update_problem
 		]));
 
 		return $column;
