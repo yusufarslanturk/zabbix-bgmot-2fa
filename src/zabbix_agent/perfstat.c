zabbix/zabbix

...G...... [ZBX-23110] reworked rwlock to semaphore

src/zabbix_agent/perfstat.c
@@ -45,27 +45,27 @@ ZBX_PERF_STAT_DATA;
 
 static ZBX_PERF_STAT_DATA	ppsd;
 static zbx_mutex_t		perfstat_access = ZBX_MUTEX_NULL;
-static zbx_mutex_t		perfstat_access_ro = ZBX_MUTEX_NULL;
-static volatile sig_atomic_t	perfstat_counter;
+static CRITICAL_SECTION		perfstat_worker_cs;
+static int			perfstat_worker_counter;
 
 static struct object_name_ref	*object_names = NULL;
 static int			object_num = 0;
 
-#define LOCK_PERFCOUNTERS	zbx_mutex_lock(perfstat_access)
-#define UNLOCK_PERFCOUNTERS	zbx_mutex_unlock(perfstat_access)
+#define LOCK_PERFCOUNTERS	zbx_mutex_glob_lock(perfstat_access)
+#define UNLOCK_PERFCOUNTERS	zbx_mutex_glob_unlock(perfstat_access)
 
 #define LOCK_PERFCOUNTERS_RO					\
-		zbx_mutex_lock(perfstat_access_ro);		\
-		perfstat_counter++;				\
-		if (1 == perfstat_counter)			\
+		EnterCriticalSection(&perfstat_worker_cs);	\
+		perfstat_worker_counter++;			\
+		if (1 == perfstat_worker_counter)		\
 			LOCK_PERFCOUNTERS;			\
-		zbx_mutex_unlock(perfstat_access_ro)
+		LeaveCriticalSection(&perfstat_worker_cs)
 #define UNLOCK_PERFCOUNTERS_RO					\
-		zbx_mutex_lock(perfstat_access_ro);		\
-		perfstat_counter--;				\
-		if (0 == perfstat_counter)			\
+		EnterCriticalSection(&perfstat_worker_cs);	\
+		perfstat_worker_counter--;			\
+		if (0 == perfstat_worker_counter)		\
 			UNLOCK_PERFCOUNTERS;			\
-		zbx_mutex_unlock(perfstat_access_ro)
+		LeaveCriticalSection(&perfstat_worker_cs)
 
 static int	perf_collector_started(void)
 {
@@ -445,10 +445,9 @@ int	init_perf_collector(zbx_threadedness_t threadedness, char **error)
 		case ZBX_SINGLE_THREADED:
 			break;
 		case ZBX_MULTI_THREADED:
-			if (SUCCEED != zbx_mutex_create(&perfstat_access, ZBX_MUTEX_PERFSTAT, error))
+			if (SUCCEED != zbx_mutex_glob_create(&perfstat_access, ZBX_MUTEX_PERFSTAT, error))
 				goto out;
-			if (SUCCEED != zbx_mutex_create(&perfstat_access_ro, ZBX_MUTEX_PERFSTAT_RO, error))
-				goto out;
+			InitializeCriticalSection(&perfstat_worker_cs);
 			break;
 		default:
 			THIS_SHOULD_NEVER_HAPPEN;
@@ -507,8 +506,8 @@ void	free_perf_collector(void)
 
 	UNLOCK_PERFCOUNTERS;
 
-	zbx_mutex_destroy(&perfstat_access);
-	zbx_mutex_destroy(&perfstat_access_ro);
+	zbx_mutex_glob_destroy(&perfstat_access);
+	DeleteCriticalSection(&perfstat_worker_cs);
 }
 
 void	collect_perfstat(void)
@@ -697,8 +696,14 @@ out:
 
 	if (NULL != counterpath)
 	{
+		PDH_STATUS	pdh_status;
+
+		LOCK_PERFCOUNTERS_RO;
+
 		/* request counter value directly from Windows performance counters */
-		PDH_STATUS pdh_status = calculate_counter_value(__func__, counterpath, perfs->lang, value);
+		pdh_status = calculate_counter_value(__func__, counterpath, perfs->lang, value);
+
+		UNLOCK_PERFCOUNTERS_RO;
 
 		if (PDH_NOT_IMPLEMENTED == pdh_status)
 			*error = zbx_strdup(*error, "Counter is not supported for this Microsoft Windows version");

yusufarslanturk/zabbix-bgmot-2fa

(cherry picked from commit 60be58370419d97bbfdda88967519f57723e8a6e)

src/zabbix_agent/perfstat.c
@@ -45,27 +45,27 @@ ZBX_PERF_STAT_DATA;
 
 static ZBX_PERF_STAT_DATA	ppsd;
 static zbx_mutex_t		perfstat_access = ZBX_MUTEX_NULL;
-static zbx_mutex_t		perfstat_access_ro = ZBX_MUTEX_NULL;
-static volatile sig_atomic_t	perfstat_counter;
+static CRITICAL_SECTION		perfstat_worker_cs;
+static int			perfstat_worker_counter;
 
 static struct object_name_ref	*object_names = NULL;
 static int			object_num = 0;
 
-#define LOCK_PERFCOUNTERS	zbx_mutex_lock(perfstat_access)
-#define UNLOCK_PERFCOUNTERS	zbx_mutex_unlock(perfstat_access)
+#define LOCK_PERFCOUNTERS	zbx_mutex_glob_lock(perfstat_access)
+#define UNLOCK_PERFCOUNTERS	zbx_mutex_glob_unlock(perfstat_access)
 
 #define LOCK_PERFCOUNTERS_RO					\
-		zbx_mutex_lock(perfstat_access_ro);		\
-		perfstat_counter++;				\
-		if (1 == perfstat_counter)			\
+		EnterCriticalSection(&perfstat_worker_cs);	\
+		perfstat_worker_counter++;			\
+		if (1 == perfstat_worker_counter)		\
 			LOCK_PERFCOUNTERS;			\
-		zbx_mutex_unlock(perfstat_access_ro)
+		LeaveCriticalSection(&perfstat_worker_cs)
 #define UNLOCK_PERFCOUNTERS_RO					\
-		zbx_mutex_lock(perfstat_access_ro);		\
-		perfstat_counter--;				\
-		if (0 == perfstat_counter)			\
+		EnterCriticalSection(&perfstat_worker_cs);	\
+		perfstat_worker_counter--;			\
+		if (0 == perfstat_worker_counter)		\
 			UNLOCK_PERFCOUNTERS;			\
-		zbx_mutex_unlock(perfstat_access_ro)
+		LeaveCriticalSection(&perfstat_worker_cs)
 
 static int	perf_collector_started(void)
 {
@@ -445,10 +445,9 @@ int	init_perf_collector(zbx_threadedness_t threadedness, char **error)
 		case ZBX_SINGLE_THREADED:
 			break;
 		case ZBX_MULTI_THREADED:
-			if (SUCCEED != zbx_mutex_create(&perfstat_access, ZBX_MUTEX_PERFSTAT, error))
+			if (SUCCEED != zbx_mutex_glob_create(&perfstat_access, ZBX_MUTEX_PERFSTAT, error))
 				goto out;
-			if (SUCCEED != zbx_mutex_create(&perfstat_access_ro, ZBX_MUTEX_PERFSTAT_RO, error))
-				goto out;
+			InitializeCriticalSection(&perfstat_worker_cs);
 			break;
 		default:
 			THIS_SHOULD_NEVER_HAPPEN;
@@ -507,8 +506,8 @@ void	free_perf_collector(void)
 
 	UNLOCK_PERFCOUNTERS;
 
-	zbx_mutex_destroy(&perfstat_access);
-	zbx_mutex_destroy(&perfstat_access_ro);
+	zbx_mutex_glob_destroy(&perfstat_access);
+	DeleteCriticalSection(&perfstat_worker_cs);
 }
 
 void	collect_perfstat(void)
@@ -697,8 +696,14 @@ out:
 
 	if (NULL != counterpath)
 	{
+		PDH_STATUS	pdh_status;
+
+		LOCK_PERFCOUNTERS_RO;
+
 		/* request counter value directly from Windows performance counters */
-		PDH_STATUS pdh_status = zbx_calculate_counter_value(__func__, counterpath, perfs->lang, value);
+		pdh_status = zbx_calculate_counter_value(__func__, counterpath, perfs->lang, value);
+
+		UNLOCK_PERFCOUNTERS_RO;
 
 		if (PDH_NOT_IMPLEMENTED == pdh_status)
 			*error = zbx_strdup(*error, "Counter is not supported for this Microsoft Windows version");