9c101b08ed0c1d7e43783746e995f5479510d584
Nikita Gogolevs <nikita.gogolevs@zabbix.com>
2023-09-08 09:44:51 +0300

    .......... [DEV-2490] added new Selenium autotest for Maintenance page
    * commit 'ed700b65b8ac1b763315f33e61fa6c11b64f7ab6':
      .......... [DEV-2490] added todo after zbx fix
      .......... [DEV-2490] added dataSource
      .......... [DEV-2490] fixed code and code formatting style
      .......... [DEV-2490] minor styling adjustments: removed quotation for timestamp(api), added extra spaces for function
      .......... [DEV-2490] created the separate folder for maintenance, updated related files
      .......... [DEV-2490] fixed the formatting, added delete all case
      .......... [DEV-2490] did minor styling adjustments, added filter element for checking the buttons
      .......... [DEV-2490] minor improvements after updating to master (used new methods)
      .......... [DEV-2490] updated the layout tests, added majoriry of requested changes
      .......... [DEV-2490] Changed the filter's label, minor data adjustment
      .......... [DEV-2490] added new tests for the Maintenance page
    
    (cherry picked from commit 893e73a63a793f94fa22efc1d1cbd55b6668854e)

--- ui/tests/selenium/SeleniumTests.php
+++ ui/tests/selenium/SeleniumTests.php
@@ -143,6 +143,10 @@ require_once dirname(__FILE__).'/macros/testFormMacrosHost.php';
 require_once dirname(__FILE__).'/macros/testFormMacrosHostPrototype.php';
 require_once dirname(__FILE__).'/macros/testFormMacrosTemplate.php';
 
+// Maintenance
+require_once dirname(__FILE__).'/maintenance/testFormMaintenance.php';
+require_once dirname(__FILE__).'/maintenance/testPageMaintenance.php';
+
 // Media types.
 require_once dirname(__FILE__).'/mediaTypes/testFormAdministrationMediaTypes.php';
 require_once dirname(__FILE__).'/mediaTypes/testFormAdministrationMediaTypeMessageTemplates.php';
@@ -250,7 +254,6 @@ require_once dirname(__FILE__).'/testPageInventory.php';
 require_once dirname(__FILE__).'/testPageTriggers.php';
 require_once dirname(__FILE__).'/testPageTriggerUrl.php';
 require_once dirname(__FILE__).'/testPageTriggerPrototypes.php';
-require_once dirname(__FILE__).'/testPageMaintenance.php';
 require_once dirname(__FILE__).'/testPageMaps.php';
 require_once dirname(__FILE__).'/testPageMassUpdateItems.php';
 require_once dirname(__FILE__).'/testPageMassUpdateItemPrototypes.php';
@@ -277,7 +280,6 @@ require_once dirname(__FILE__).'/filterTabs/testFormFilterHosts.php';
 require_once dirname(__FILE__).'/filterTabs/testFormFilterLatestData.php';
 require_once dirname(__FILE__).'/filterTabs/testFormFilterProblems.php';
 require_once dirname(__FILE__).'/testFormLogin.php';
-require_once dirname(__FILE__).'/testFormMaintenance.php';
 require_once dirname(__FILE__).'/testFormMap.php';
 require_once dirname(__FILE__).'/testFormSetup.php';
 require_once dirname(__FILE__).'/testFormSysmap.php';
