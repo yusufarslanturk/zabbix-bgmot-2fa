bd7ba8963f8be59184bf391c00e970d846cd07e5
Dmitrijs Fofanovs <dmitrijs.fofanovs@zabbix.com>
2023-10-23 16:34:19 +0300

    ..F....... [ZBX-23429] refactored overflow-ellipsis in latest data

--- ui/app/partials/monitoring.latest.view.html.php
+++ ui/app/partials/monitoring.latest.view.html.php
@@ -28,7 +28,7 @@ $form = (new CForm('GET', 'history.php'))
 	->setName('items')
 	->addItem(new CVar('action', HISTORY_BATCH_GRAPH));
 
-$table = (new CTableInfo())->addClass(ZBX_STYLE_OVERFLOW_ELLIPSIS);
+$table = (new CTableInfo())->addStyle('table-layout: fixed;');
 
 // Latest data header.
 $col_check_all = new CColHeader(
@@ -267,8 +267,10 @@ foreach ($data['items'] as $itemid => $item) {
 
 		$table_row = new CRow([
 			$checkbox,
-			$host_name_container,
-			(new CCol([$item_name, $item_key]))->addClass($state_css),
+			(new CCol($host_name_container))->addClass(ZBX_STYLE_OVERFLOW_ELLIPSIS),
+			(new CCol([$item_name, $item_key]))
+				->addClass($state_css)
+				->addClass(ZBX_STYLE_OVERFLOW_ELLIPSIS),
 			(new CCol($item_delay))->addClass($state_css),
 			(new CCol($item_history))->addClass($state_css),
 			(new CCol($item_trends))->addClass($state_css),
@@ -284,7 +286,7 @@ foreach ($data['items'] as $itemid => $item) {
 	else {
 		$table_row = new CRow([
 			$checkbox,
-			$host_name_container,
+			(new CCol($host_name_container))->addClass(ZBX_STYLE_OVERFLOW_ELLIPSIS),
 			(new CCol($item_name))->addClass($state_css),
 			(new CCol($last_check))->addClass($state_css),
 			(new CCol($last_value))->addClass($state_css),
