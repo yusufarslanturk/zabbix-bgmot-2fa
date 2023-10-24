b3ab8fc0b32825654e3a39ef118f96d67c1fd22c
Alexander Shubin <aleksandrs.subins@zabbix.com>
2023-10-24 16:22:27 +0300

    ..F....... [ZBX-23429] fixed code style

--- ui/app/partials/monitoring.latest.view.html.php
+++ ui/app/partials/monitoring.latest.view.html.php
@@ -28,7 +28,7 @@ $form = (new CForm('GET', 'history.php'))
 	->setName('items')
 	->addItem(new CVar('action', HISTORY_BATCH_GRAPH));
 
-$table = (new CTableInfo())->addStyle(ZBX_STYLE_LIST_TABLE_FIXED);
+$table = (new CTableInfo())->addClass(ZBX_STYLE_LIST_TABLE_FIXED);
 
 // Latest data header.
 $col_check_all = new CColHeader(
