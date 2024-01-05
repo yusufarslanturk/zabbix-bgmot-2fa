c97737f5ca866d971faecccad4e03726f7a878fd
Tomass Olups <tomass.olups@zabbix.com>
2024-01-05 16:03:49 +0200

    .......... [ZBX-23262] updated testFormLogin autotest first data set error message

--- ui/tests/selenium/testFormLogin.php
+++ ui/tests/selenium/testFormLogin.php
@@ -34,7 +34,7 @@ class testFormLogin extends CWebTest {
 					'login' => 'LDAP user',
 					'password' => 'zabbix12345',
 					'expected' => TEST_BAD,
-					'error_message' => 'LDAP authentication is disabled.'
+					'error_message' => 'Incorrect user name or password or account is temporarily blocked.'
 				]
 			],
 			[
