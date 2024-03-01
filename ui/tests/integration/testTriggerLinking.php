3cf8434fb2c32d72207d17aedf86e05a658b5935
Artjoms Rimdjonoks <artjoms.rimdjonoks@zabbix.com>
2024-03-01 09:54:29 +0200

    ........S. [ZBX-24049] removed commented line

--- ui/tests/integration/testTriggerLinking.php
+++ ui/tests/integration/testTriggerLinking.php
@@ -471,7 +471,6 @@ class testTriggerLinking extends CIntegrationTest {
 		$this->startComponent(self::COMPONENT_SERVER);
 		sleep(1);
 		$this->startComponent(self::COMPONENT_AGENT);
-		/*$this->waitForLogLineToBePresent(self::COMPONENT_SERVER, ['End of DBregister_host_active():SUCCEED']);*/
 		$this->waitForLogLineToBePresent(self::COMPONENT_SERVER, 'End of zbx_db_copy_template_elements()', true, 120);
 		$this->checkTriggersCreate();
 		$this->setupActionToLinkTemplateXThatConflictsWithAlreadyLinkedTemplates();
