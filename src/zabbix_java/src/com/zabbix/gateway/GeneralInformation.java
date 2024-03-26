d00264512a9f47ad8e10dcb4478d8c1b84728d7f
Larisa Grigorjeva <larisa.grigorjeva@zabbix.com>
2024-03-26 14:14:53 +0200

    .......... [ZBXNEXT-826] updated release version to 6.4.14rc1

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -24,7 +24,7 @@ class GeneralInformation
 	static final String APPLICATION_NAME = "Zabbix Java Gateway";
 	static final String REVISION_DATE = "25 March 2024";
 	static final String REVISION = "{ZABBIX_REVISION}";
-	static final String VERSION = "6.4.13";
+	static final String VERSION = "6.4.14rc1";
 
 	static void printVersion()
 	{
