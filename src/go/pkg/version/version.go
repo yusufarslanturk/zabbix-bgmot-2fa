ea7c15f63c88e40efdcc5a23e7cb1b466bde7e42
Continuous Integration <ci@zabbix.com>
2024-04-15 13:25:56 +0300

    .......... [ZBXNEXT-826] updated release date for 6.4.14rc1

--- src/go/pkg/version/version.go
+++ src/go/pkg/version/version.go
@@ -27,7 +27,7 @@ import (
 )
 
 const (
-	ZABBIX_REVDATE          = "25 March 2024"
+	ZABBIX_REVDATE          = "15 April 2024"
 	ZABBIX_VERSION_MAJOR    = 6
 	ZABBIX_VERSION_MINOR    = 4
 	ZABBIX_VERSION_PATCH    = 14
