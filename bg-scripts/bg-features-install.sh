d7e06ede9ff421dbd6a014b75f5baccd290bfc89
Evgeny Yurchenko <ey@bgmot.com>
2024-04-24 07:49:54 -0400

    Remove CUserGroup.php from bg-features-install.sh scripts, we do not need to copy it.

--- bg-scripts/bg-features-install.sh
+++ bg-scripts/bg-features-install.sh
@@ -113,8 +113,6 @@ cp ${ZABBIX_INSTALL_PATH}/include/classes/api/CAudit.php ${ZABBIX_INSTALL_PATH}/
 cp CAudit.php ${ZABBIX_INSTALL_PATH}/include/classes/api/
 cd services
 cp CAdUserGroup.php ${ZABBIX_INSTALL_PATH}/include/classes/api/services/
-cp ${ZABBIX_INSTALL_PATH}/include/classes/api/services/CAuditLog.php ${ZABBIX_INSTALL_PATH}/include/classes/api/services/CAuditLog.php-`date +%s`.bak
-cp CAuditLog.php ${ZABBIX_INSTALL_PATH}/include/classes/api/services/
 cp ${ZABBIX_INSTALL_PATH}/include/classes/api/services/CUser.php ${ZABBIX_INSTALL_PATH}/include/classes/api/services/CUser.php-`date +%s`.bak
 cp CUser.php ${ZABBIX_INSTALL_PATH}/include/classes/api/services/
 cp ${ZABBIX_INSTALL_PATH}/include/classes/api/services/CUserGroup.php ${ZABBIX_INSTALL_PATH}/include/classes/api/services/CUserGroup.php-`date +%s`.bak
