7267f05d41ebd38187d0f8c45bad099eee734162
Andrejs Griščenko <andrejs.griscenko@zabbix.com>
2024-05-30 13:33:54 +0300

    ..F....... [ZBX-22623] moved including of jQuery based JS files below

--- ui/app/views/monitoring.dashboard.view.php
+++ ui/app/views/monitoring.dashboard.view.php
@@ -24,8 +24,6 @@
  * @var array $data
  */
 
-$this->addJsFile('class.coverride.js');
-$this->addJsFile('class.crangecontrol.js');
 $this->addJsFile('class.dashboard.js');
 $this->addJsFile('class.dashboard.page.js');
 $this->addJsFile('class.dashboard.widget.placeholder.js');
@@ -35,7 +33,6 @@ $this->addJsFile('class.widget.js');
 $this->addJsFile('class.widget.inaccessible.js');
 $this->addJsFile('class.widget.iterator.js');
 $this->addJsFile('class.widget.paste-placeholder.js');
-$this->addJsFile('layout.mode.js');
 
 if (array_key_exists('error', $data)) {
 	show_error_message($data['error']);
@@ -45,12 +42,15 @@ if (array_key_exists('error', $data)) {
 
 $this->addJsFile('class.calendar.js');
 $this->addJsFile('class.cnavtree.js');
+$this->addJsFile('class.coverride.js');
+$this->addJsFile('class.crangecontrol.js');
 $this->addJsFile('class.csvggraph.js');
 $this->addJsFile('class.svg.canvas.js');
 $this->addJsFile('class.svg.map.js');
 $this->addJsFile('colorpicker.js');
 $this->addJsFile('flickerfreescreen.js');
 $this->addJsFile('gtlc.js');
+$this->addJsFile('layout.mode.js');
 $this->addJsFile('leaflet.js');
 $this->addJsFile('leaflet.markercluster.js');
 $this->addJsFile('class.geomaps.js');
