627067136ca689d689c92240eca8c7c545314921
Miks Kronkalns <miks.kronkalns@zabbix.com>
2024-06-13 13:59:24 +0300

    ..F....... [ZBX-24588] fixed case when null is passed to vault constructor

--- ui/include/classes/vaults/CVaultCyberArk.php
+++ ui/include/classes/vaults/CVaultCyberArk.php
@@ -49,11 +49,11 @@ class CVaultCyberArk extends CVault {
 	 */
 	protected $key_file;
 
-	public function __construct(string $api_endpoint, string $db_path, string $cert_file, string $key_file) {
+	public function __construct(string $api_endpoint, string $db_path, ?string $cert_file, ?string $key_file) {
 		$this->api_endpoint = rtrim(trim($api_endpoint), '/');
 		$this->db_path = trim($db_path);
-		$this->cert_file = trim($cert_file);
-		$this->key_file = trim($key_file);
+		$this->cert_file = trim($cert_file ?? '');
+		$this->key_file = trim($key_file ?? '');
 	}
 
 	public function validateParameters(): bool {
